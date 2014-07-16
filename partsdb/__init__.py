2ea81b68539f1bda790ced09520b8d453ef11498
Josh Friend <josh@fueledbycaffeine.com>
2014-07-15 21:08:22 -0400

    removed unused file logging to fix #1

--- partsdb/__init__.py
+++ partsdb/__init__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -8,15 +10,5 @@ app = Flask('partsdb')
 app.config.from_object('config')
 db = SQLAlchemy(app)
 Markdown(app)
-
-if not app.debug:
-    import logging
-    from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('tmp/partsdb.log', 'a', 1 * 1024 * 1024, 10)
-    file_handler.setLevel(logging.INFO)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    app.logger.addHandler(file_handler)
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('PartsDB Startup...')
 
 from partsdb import views, models, filters
